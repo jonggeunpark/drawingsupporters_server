23a348e7b51df4a6f151e90700723e51f6ef95c5
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-06-18 15:05:53 +0900

    init

---
+++ src/test/java/com/drawing/drawing/DrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drawing.drawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
