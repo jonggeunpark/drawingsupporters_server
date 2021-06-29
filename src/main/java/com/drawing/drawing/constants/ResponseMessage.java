5f364835d884639c76533392af68b6dff8790180
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-06-29 20:15:10 +0900

    Add : Constants

---
+++ src/main/java/com/drawing/drawing/constants/ResponseMessage.java
@@ -0,0 +1,21 @@
+package com.drawing.drawing.constants;
+
+public class ResponseMessage {
+
+    public static final String CREATE_MENTEE = "회원가입 성공";
+    public static final String LOGIN_SUCCESS = "로그인 성공";
+    public static final String EMAIL_CHECK_SUCCESS = "이메일 중복 확인 성공";
+    public static final String NICKNAME_CHECK_SUCCESS = "닉네임 중복 확인 성공";
+    public static final String READ_USER_INFO_SUCCESS = "유저 정보 확인 성공";
+    public static final String READ_ALL_FEEDBACK = "피드백 전체 조회 성공";
+    public static final String READ_FEEDBACK = "피드백 상세 조회 성공";
+    public static final String READ_ALL_DRAWING = "피드백 요청 전체 조회 성공";
+    public static final String READ_DRAWING = "피드백 요청 상세 조회 성공";
+    public static final String CREATE_DRAWING = "피드백 요청 생성 성공";
+
+
+    public static final String DUPLICATE_MENTEE = "이미 등록되어있는 이메일입니다.";
+    public static final String INVALID_PASSWORD = "비밀번호가 유효하지 않습니다.";
+    public static final String NOT_FOUND = "Not Found";
+
+}
