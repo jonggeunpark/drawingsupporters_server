1d7b16ab89fcf4da68e368d48cfbfead832209fc
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-06-29 20:25:03 +0900

    Add : Token Dto

---
+++ src/main/java/com/drawing/drawing/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package com.drawing.drawing.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String accessToken;
+    private String refreshToken;
+}
