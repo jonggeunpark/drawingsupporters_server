e32b722c68efd50478cafd1bb16b54c3df575f05
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-07-06 22:49:31 +0900

    Update : Change Comment from single-line to multi-line

--- src/main/java/com/drawing/drawing/controller/FeedbackController.java
+++ src/main/java/com/drawing/drawing/controller/FeedbackController.java
@@ -3,19 +3,25 @@ package com.drawing.drawing.controller;
 import com.drawing.drawing.constants.Message;
 import com.drawing.drawing.constants.ResponseMessage;
 import com.drawing.drawing.constants.StatusCode;
+import com.drawing.drawing.dto.Drawing.DrawingRequestDto;
 import com.drawing.drawing.dto.Feedback.DetailFeedbackDto;
+import com.drawing.drawing.dto.Feedback.FeedbackReqeustDto;
 import com.drawing.drawing.dto.Feedback.SimpleFeedbackDto;
+import com.drawing.drawing.exception.UnauthorizedException;
 import com.drawing.drawing.service.FeedbackService;
+import com.drawing.drawing.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,9 +29,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FeedbackController {
 
+    private final UserService userService;
     private final FeedbackService feedbackService;
 
-    //피드백 전체 조회
+    /**
+     * 피드백 전체 조회
+     * METHOD : GET
+     * URI : /api/feedback
+     * 권한 : 없음
+     */
     @GetMapping()
     private ResponseEntity<Message> readAllFeedback() {
 
@@ -35,7 +47,12 @@ public class FeedbackController {
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
-    //피드백 상세 조회
+    /**
+     * 피드백 상세 조회
+     * METHOD : GET
+     * URI : /api/feedback/{feedbackId}
+     * 권한 : 로그인
+     */
     @GetMapping("/{feedbackId}")
     private ResponseEntity<Message> readFeedback(@PathVariable("feedbackId") Long feedbackId) {
 
