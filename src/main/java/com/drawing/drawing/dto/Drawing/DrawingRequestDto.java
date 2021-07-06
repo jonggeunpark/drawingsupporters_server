8c91f903b4367a463f45ab8c54ffcdf310f72828
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-07-06 20:43:51 +0900

    Update : Request type from LocalDateTime to String

--- src/main/java/com/drawing/drawing/dto/Drawing/DrawingRequestDto.java
+++ src/main/java/com/drawing/drawing/dto/Drawing/DrawingRequestDto.java
@@ -9,9 +9,12 @@ import com.drawing.drawing.entity.Mento;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,18 +31,20 @@ public class DrawingRequestDto {
     private MultipartFile file;
 
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-    private LocalDateTime end_time;
+    private String end_time;
     private String phone_number;
 
 
     public Drawing toEntity(Mentee mentee, String uuid, String filename) {
+
+        LocalDateTime dateTime = LocalDateTime.parse(end_time, DateTimeFormatter.ISO_DATE);
 
         return Drawing.builder()
                 .mentee(mentee)
                 .title(title)
                 .description(description)
                 .feedbackType(String.join(",", feedback_file_type))
-                .endDate(end_time)
+                .endDate(dateTime)
                 .priceLowerLimit(start)
                 .priceUpperLimit(end)
                 .uuid(uuid)
