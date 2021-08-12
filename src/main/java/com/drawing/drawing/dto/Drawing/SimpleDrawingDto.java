48503c4cae3de6703f03fb1721d4929c73903237
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-08-12 14:54:45 +0900

    Update : change response for read drawing list API

--- src/main/java/com/drawing/drawing/dto/Drawing/SimpleDrawingDto.java
+++ src/main/java/com/drawing/drawing/dto/Drawing/SimpleDrawingDto.java
@@ -5,18 +5,20 @@ import com.drawing.drawing.entity.Drawing;
 import com.drawing.drawing.entity.Feedback;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 
 @AllArgsConstructor
 @Getter
 public class SimpleDrawingDto {
 
     private Long id;
+    private String nickname;
     private String title;
     private String thumbnail;
+    private String feedback_type;
+
 
     public static SimpleDrawingDto of(Drawing drawing, String storage) {
 
-        return new SimpleDrawingDto(drawing.getId(), drawing.getTitle(), storage + "/" + drawing.getUuid() + drawing.getFilename());
+        return new SimpleDrawingDto(drawing.getId(), drawing.getMentee().getNickname(), drawing.getTitle(), storage + "/" + drawing.getUuid() + drawing.getFilename(), drawing.getFeedbackType());
     }
 }
